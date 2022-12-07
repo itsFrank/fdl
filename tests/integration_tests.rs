1174114cbf55766c4cc886b01168a3f0c991765d
itsFrank <frankobrien11@yahoo.ca>
2022-12-06 21:31:37 -0800

    Added test with nested children and props

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -98,3 +98,51 @@ fn parse_a_complex_source() {
         PropValue::String("I'm a String".to_string())
     );
 }
+
+#[test]
+fn parse_nested_things_with_props() {
+    let source = r#"
+        thing "Root" {
+            int int_prop = 12
+            
+            thing "Child1" {
+                bool bool_prop = true
+            }
+
+            thing "Child2" {
+                float float_prop = 12.1
+            }
+        }
+    "#;
+
+    let lexer = Lexer::new(source);
+    let parser = Parser::from_tokens(lexer).unwrap();
+
+    assert_eq!(parser.things.len(), 1);
+    let thing = parser.things.get("Root").unwrap();
+    assert_eq!(thing.name, "Root");
+    assert_eq!(thing.things.len(), 2);
+    assert_eq!(thing.props.len(), 1);
+
+    let int_prop = thing.props.get("int_prop").unwrap();
+    assert_eq!(int_prop.name, "int_prop");
+    assert_eq!(int_prop.value, PropValue::Int(12));
+
+    let child1 = thing.things.get("Child1").unwrap();
+    assert_eq!(child1.name, "Child1");
+    assert_eq!(child1.things.len(), 0);
+    assert_eq!(child1.props.len(), 1);
+
+    let bool_prop = child1.props.get("bool_prop").unwrap();
+    assert_eq!(bool_prop.name, "bool_prop");
+    assert_eq!(bool_prop.value, PropValue::Bool(true));
+
+    let child2 = thing.things.get("Child2").unwrap();
+    assert_eq!(child2.name, "Child2");
+    assert_eq!(child2.things.len(), 0);
+    assert_eq!(child2.props.len(), 1);
+
+    let float_prop = child2.props.get("float_prop").unwrap();
+    assert_eq!(float_prop.name, "float_prop");
+    assert_eq!(float_prop.value, PropValue::Float(12.1));
+}
